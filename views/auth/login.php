25bc579cf9a182abb548d1b5975221211d988adb
zpect <jvbertaggia@gmail.com>
2024-08-17 21:35:25 -0400

    cambios

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,5 +1,5 @@
 <h1 class="nombre-pagina">Login</h1>
-<p class="descripcion-pagina">Inicia sesion con tus datosasd</p>
+<p class="descripcion-pagina">Inicia sesion con tus datos</p>
 
 <?php include_once __DIR__ . "/../templates/alertas.php" ?>
 
